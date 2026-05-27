946c84f9d250440c4d04fbd22829730ce5411e0a
차희광 <chahuigwang@gmail.com>
2026-05-27 17:09:01 +0900

    Swagger 초기 설정

---
+++ BE/src/main/java/com/chapyo/common/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.chapyo.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("ChaPyo API")
+                .version("1.0.0")
+                .description("ChaPyo REST API 명세"));
+    }
+}

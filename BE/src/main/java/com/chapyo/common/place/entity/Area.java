047dd3fad0c56921521586276a467262ead426e4
차희광 <chahuigwang@gmail.com>
2026-05-28 15:15:07 +0900

    feat: Places Entity 클래스 작성

---
+++ BE/src/main/java/com/chapyo/common/place/entity/Area.java
@@ -0,0 +1,13 @@
+package com.chapyo.common.place.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Area {
+    private String areaCode;
+    private String name;
+}
---
+++ BE/src/main/java/com/chapyo/common/place/entity/Place.java
@@ -0,0 +1,29 @@
+package com.chapyo.common.place.entity;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Place {
+    private Long placeId;
+    private String contentId;
+    private String title;
+    private String addr1;
+    private String addr2;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+    private String firstImage1;
+    private String firstImage2;
+    private String areaCode;
+    private String districtCode;
+    private String categoryCode1;
+    private String categoryCode2;
+    private String zipcode;
+    private String tel;
+    private String overview;
+}
